22cb711645d63b3caaf3fa146c372bbef8767f95
alvaro vidaurre <cheft96.esparragos@gmail.com>
2022-04-03 22:51:34 -0400

    first commit

---
+++ principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout <<"hola marcianos "<< endl;
+    cout <<"nasheeeeeeee"<< endl;
+
+    cout <<"nasheeeeeSCVDFBGVHBNM,eee"<< endl;
+    system("pause");
+    return 0;
+}
