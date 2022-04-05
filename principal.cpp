4e9c479551259903dd3e68b0612dca3a9ae98388
alvaro vidaurre <cheft96.esparragos@gmail.com>
2022-04-04 22:12:26 -0400

    cambios

--- principal.cpp
+++ principal.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
 using namespace std;
+int main() {
+	char v = 'v';
+	char s = 's';
 
-int main(){
 
-    cout <<"hola marcianos "<< endl;
-    cout <<"nasheeeeeeee"<< endl;
+		while (v == 'v') {
+			cout << "hola mi nombre es Alvaro" << endl;
+			cout << "soy de Tupiza situado en el departamento de Potosi pero naci en la localidad de Oploca" << endl;
+			cout << "me mude a tupiza alos 5 años y vivi la mayor parte en tupiza" << endl;
+			cout << "los nombres de mis padres son LUCIA  y GROVER " << endl;
+			cout << "actualmente vivo en alquiler en la ciudad de  sucre para cursar la carrera de ING.SISTEMAS " << endl;
+			cout << "y me gustan los videojuegos!!! :D " << endl;
+			cout << "presione (v) para seguir \npara salir (s)" << endl;
 
-    cout <<"nasheeeeeSCVDFBGVHBNM,eee"<< endl;
-    system("pause");
-    return 0;
+			cin >> v;
+		}
+	return 0;
 }
